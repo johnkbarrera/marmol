860b55b4b375fd46430cb2d9947cbee0a9e20c22
John k. Barrera <johnkevinbarrera@gmail.com>
2017-08-10 19:54:13 -0500

    c1

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/marmool/'; //localhost:8080/marmool/      x          www.jkn.com
+$config['base_url'] = 'http://localhost:80/marmol/'; //localhost:8080/marmool/      x          www.jkn.com
 
 /*
 |--------------------------------------------------------------------------
